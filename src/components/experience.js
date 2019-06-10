5e4651a0e795349406eaf7f8cbb162423d7d3ee4
user <user@users-iMac.local>
2019-06-10 22:27:19 +0100

    styling skills

--- src/components/experience.js
+++ src/components/experience.js
@@ -9,9 +9,10 @@ function Experience() {
             <h3>Barley House Agency</h3>
             <p>Current Role: <strong>Web Developer</strong> | Cuffley, Hertfordshire | August 2018 - Current</p>
             <ul className="experience-list"> 
-                <li>Creating and Developing Bespoke wordpress sites from stratch, custom built themes</li>
+                <li>Building custom designed websites and web applications using HTML,SCSS, Javascript & jQuery</li>
 
-                <li>Workflow tools included: HTML, Scss, Wordpress PHP, jQuery, Glup and Git</li>
+                <li>Workflow tools included: HTML, Scss, Wordpress, jQuery, Glup and Git</li>
+                <li>Example: <a rel="noopener noreferrer" target='_blank' href='https://www.allanandbertram.com/'>Allan & Bertram</a></li>
             </ul>
 
             <hr />
@@ -20,9 +21,9 @@ function Experience() {
             <p><strong>Junior Front-End Web Developer</strong> | Milton Keynes | May 2017 - August 2018</p>
 
             <ul className="experience-list">
-                <li>Build Web pages, using HTML,CSS, Javascript, jQuery, JSON, Google maps API.</li>
-                <li>Assisted the Digital lead in building an Angular 4 &amp; NodeJS file transfer application to assist the design and sales team.</li>
-                <li>Create Websites in Wordpress and modify photos to be used in websites using Adobe Photoshop and create wireframes.</li>
+                <li>Using HTML, CSS & Scss, jQuery & Javascript, Google Maps API, I was involved in the design and development of microsites, brochure sites, and e-commerce websites and HTML emails.</li>
+
+                <li>Assisted the digital lead in building an Angular 4 & NodeJS file transfer application to assist the design and sales team.</li>
             </ul>
 
             <hr />
@@ -31,10 +32,11 @@ function Experience() {
             <p><strong>Junior Front-End Web Developer</strong> | London | Feb 2017 - May 2017</p>
 
             <ul className="experience-list">
-                <li>Developed, enhanced and maintained company’s websites using HTML, Sass, and Javascript with libaries such as jQuery and React.
-                </li>
-                <li>Fixed front end bugs (HTML, CSS)</li>
-                <li>Ran some code meetings at lunchtime for people who were interested in learning.</li>
+                <li>Developed, enhanced & maintained company’s websites using HTML, Sass, & Javascript with libraries such as jQuery and ReactJS.</li>
+
+                <li>Daily stand ups every morning, worked with tickets on Trello.</li>
+
+                <li> Ran code meetings at lunchtime for people who were interested in learning.</li>
             </ul>
             <p>Workflow tools included: HTML, CSS, Scss Bootstrap, jQuery, React, Gulp and Git</p>
 
@@ -46,8 +48,8 @@ function Experience() {
             <ul className="experience-list">
                 <li>Created bespoke websites from scratch and working closely with the Web designers.</li>
                 <li>Hand coding front-end code responsive websites that work on all devices, with HTML/CSS, Javascript &amp; jQuery.</li>
-                <li>Example: <a rel="noopener noreferrer" target='_blank' href='http://www.ladiescircle.co.uk/'>Ladies Circle</a></li>
-                <li>Example: <a rel="noopener noreferrer" target="_blank" href="http://charlottesandersondesign.co.uk/">Charlotte Sanderson Garden Design</a></li>
+                <li>Example: <a rel="noopener noreferrer" target='_blank' href='http://www.ladiescircle.co.uk/'>Ladies Circle</a>, &amp;
+                <a rel="noopener noreferrer" target="_blank" href="http://charlottesandersondesign.co.uk/"> Charlotte Sanderson Garden Design</a></li>
             </ul>
         </section >
     )
--- src/components/skills.js
+++ src/components/skills.js
@@ -1,24 +1,36 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHtml5, faCss3, faJs, faWordpress } from '@fortawesome/free-brands-svg-icons';
 
+
+// faHtml5, faCss3, faJs, fa 
 function Skills() {
     return (
         <section className="skills-section">
 
-            <div class="skill__content"> 
-                <h3>Skills</h3>
+            <div className="skill__content"> 
+                <h3>My Skills</h3>
                     <ul className="skills-list">
-                        <li>Responsive HTML/CSS</li>
-                        <li>Scss</li>
-                        <li>Vanilla Javascript</li>
-                        <li>Wordpress</li>
+                        <li><FontAwesomeIcon icon={faHtml5}> </FontAwesomeIcon> HTML5 </li>
+                        <li><FontAwesomeIcon icon={faCss3}> </FontAwesomeIcon> Responsive CSS/Scss</li>
+                        <li><FontAwesomeIcon icon={faJs}> </FontAwesomeIcon> Javascript</li>
+                        <li><FontAwesomeIcon icon={faWordpress}> </FontAwesomeIcon> Wordpress</li>
+                    </ul>
+                    <ul className="skills-list">
                         <li className="sub"><strong>Frameworks & Libraries</strong></li>
                         <li>jQuery</li>
                         <li>React</li>
                         <li>Angular V2</li>
+                    </ul>
+                    <ul className="skills-list">
                         <li className="sub"><strong>Tools</strong></li>
                         <li>Git</li>
+                        <li>Gulp</li>
                         <li>Command Line</li>
-                        <li className="sub">Design Tools</li>
+                    </ul>
+                    <ul className="skills-list">
+                        <li className="sub">Design Skills</li>
+                        <li>Wireframing</li>
                         <li>Photoshop</li>
                     </ul>
                 
